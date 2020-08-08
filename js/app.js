e650673330c2d862127a8db1ff455196cd34b8ec
Mastermxx <62095914+Mastermxx@users.noreply.github.com>
2020-08-08 15:50:23 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
+/******************************************
+ Treehouse FSJS Techdegree:
+ project 4 - OOP Game App
+ Script by Mark Reijgwart
+ I am aiming for a "Exceeds Expectations" grade.
+ If I don't get this grade I would like to redo it.
+ ******************************************/
 
 const game = new Game();
 startGameButton.addEventListener('click', function() {
