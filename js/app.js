99cb2fdddec0a8ab8cb26f0955155ff172455303
Mastermxx <62095914+Mastermxx@users.noreply.github.com>
2020-08-29 02:10:26 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,28 +6,8 @@
  If I don't get this grade I would like to redo it.
  ******************************************/
 
-
-
-
 startGameButton.addEventListener('click', function() {
     const game = new Game();
     game.startGame();
     game.handleInteraction();
 })
-
-// For press on the physical keyboard add event listener.
-// The input will be for example: "keyR" so I remove key from the string and make it lowercase.
-// So it can be compared like the onscreen keyboard keys.
-// On keydown send input to registerInput().
-document.addEventListener('keydown', (event) => {
-    if (game.isAvailable) {
-        const currentKey = event.key;
-        const regex = /[a-z]/g;
-        console.log(currentKey)
-
-        if (currentKey.match(regex) && currentKey.length === 1) {
-            console.log('correct')
-            game.registerInput(currentKey);
-        }
-    }
-});
