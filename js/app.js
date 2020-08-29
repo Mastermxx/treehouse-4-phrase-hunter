149e027f7016e2de8795f58010ad3faf95bdd15a
Mastermxx <62095914+Mastermxx@users.noreply.github.com>
2020-08-29 02:37:41 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,8 +6,29 @@
  If I don't get this grade I would like to redo it.
  ******************************************/
 
+let game;
+
+function clickHandler(event){
+    const btn = event.target.closest('button')
+    if(btn) game.handleInteraction(btn)
+}
+
+function keydownHandler(event){
+    if(game.isAvailable){
+        const currentKey = event.key;
+        const regex = /[a-z]/g;
+
+        if (currentKey.match(regex)
+            && currentKey.length === 1) {
+            const element = keys.find(k => k.innerHTML === currentKey);
+            game.handleInteraction(element);
+        }
+    }
+}
+qwerty.addEventListener('click', clickHandler)
+document.addEventListener('keydown', keydownHandler)
+
 startGameButton.addEventListener('click', function() {
-    const game = new Game();
+    game = new Game();
     game.startGame();
-    game.handleInteraction();
 })
